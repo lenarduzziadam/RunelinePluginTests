6eb10489e5d73344c96d2387e4ccdfbcd507363d
lenarduzziadam <lenarduzziadam@gmail.com>
2023-12-21 20:08:49 -0600

    made changes to main

--- src/test/java/com/example/ExamplePluginTest.java
+++ src/test/java/com/example/ExamplePluginTest.java
@@ -2,12 +2,36 @@ package com.example;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
+import java.util.logging.Logger;
 
 public class ExamplePluginTest
 {
-	public static void main(String[] args) throws Exception
-	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
-		RuneLite.main(args);
-	}
+    private static final Logger logger = Logger.getLogger(ExamplePluginTest.class.getName());
+
+    public static void main(String[] args)
+    {
+        try {
+            // Load the ExamplePlugin
+            ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+            logger.info("ExamplePlugin loaded successfully.");
+
+            // Process custom arguments
+            processArguments(args);
+
+            // Start RuneLite
+            RuneLite.main(args);
+        } catch (Exception e) {
+            logger.severe("An error occurred while starting the ExamplePlugin: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private static void processArguments(String[] args) {
+        // Process any custom arguments for your plugin here
+        // For example, configuration settings, debug modes, etc.
+        for (String arg : args) {
+            logger.info("Processing argument: " + arg);
+            // Add your argument processing logic here
+        }
+    }
 }
